a94e93ce02b422ccabd5c86fe03688b23ed1f334
Yashwant Yadav <yadavyeshwant6166@gmail.com>
2024-09-13 21:44:01 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import  Login from '../src/components/LoginPage';
-import  Report from '../src/components/Report';
-import  EventSummary from '../src/components/EventSummary';
 import Taskbar from '../src/components/Taskbar';
 function App() {
   return (
